13ccb360f1c60c0ec98029e383b602eaf1b617b2
hrajmund <hubairajmund0507@gmail.com>
2023-07-07 22:38:34 +0200

    Player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    private float horizontal;
+    private float speed = 8f;
+
+    private Rigidbody2D rb;
+
+    void Start() {
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //rb.AddForce(new Vector2(0,1));
+        rb.velocity = new Vector2(0.0f, 2.0f);
+    }
+
+    private void FixedUpdate(){
+
     }
 }
