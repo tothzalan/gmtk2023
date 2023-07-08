62622b4a6af92bbf6df55550ba9ed517ba515260
hrajmund <hubairajmund0507@gmail.com>
2023-07-08 11:05:26 +0200

    Car's sprite flipped

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    private Vector2 spawnPos;
+    public Rigidbody2D rb;
+    float scaleY;
+
     // Start is called before the first frame update
     void Start()
     {
+        spawnPos = transform.position;
+        scaleY = transform.localScale.y;
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    private void FixedUpdate(){
-        if(transform.position.y > 5){
-            rb.velocity = new Vector2(0.0f, -((Time.frameCount * 0.01f) * 2.0f));
-        }else if(transform.position.y < -5){
-            rb.velocity = new Vector2(0.0f, -((Time.frameCount * 0.01f) * 2.0f));
-        }else{
-            rb.velocity = new Vector2(-(Time.frameCount * 0.01f) * 2.0f, 0.0f);
+        if(spawnPos.y > 2){
+            rb.velocity = new Vector2(0.0f, -5.0f);
+            transform.localScale = new Vector2(transform.localScale.x, (-1)*scaleY);
+        }
+        else if(spawnPos.y < -2){
+            rb.velocity = new Vector2(0.0f, 5.0f);
+            transform.localScale = new Vector2(transform.localScale.x, scaleY);
         }
     }
 }
