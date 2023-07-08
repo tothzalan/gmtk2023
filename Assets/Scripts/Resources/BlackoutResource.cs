5b0221e5f1c0a2ca0bc1247ca05032f5ad2e76e9
tothzalan <toth.zalan03@gmail.com>
2023-07-08 11:45:02 +0200

    Other resources converted to singletons

--- Assets/Scripts/Resources/BlackoutResource.cs
+++ Assets/Scripts/Resources/BlackoutResource.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class BlackoutResource : Resource
 {
+    private static BlackoutResource instance;
 
     public BlackoutResource(int numberOwned) : base(numberOwned) 
     {
     }
 
+    public static BlackoutResource GetInstance()
+    {
+        if(instance == null)
+            throw new InvalidOperationException("You have to call the Init method first");
+        return instance;
+    }
+
+    public static BlackoutResource Init(int numberOwned)
+    {
+        if(instance != null)
+            throw new InvalidOperationException("You have already initialized the Singleton");
+        instance = new BlackoutResource(numberOwned);
+        return instance;
+    }
+
+    public override string InventoryTag()
+    {
+        return "BlackoutInventory";
+    }
 }
