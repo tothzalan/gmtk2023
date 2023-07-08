2cdf3d3b33df90aba2a7cf8f791ac074d4181a21
hrajmund <hubairajmund0507@gmail.com>
2023-07-08 22:41:53 +0200

    Traffic Cone Animation

--- Assets/Scripts/PropScripts/CollectibleBuoy.cs
+++ Assets/Scripts/PropScripts/CollectibleBuoy.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 
 namespace PropScripts
 {
@@ -6,9 +7,16 @@ namespace PropScripts
     {
         [NonSerialized]
         public bool isPlacedByPlayer;
+        private Animator anim;
+        private static readonly int PickedUp = Animator.StringToHash("PickedUp");
+
         public override bool CanInteract()
         {
             return !isPlacedByPlayer;
+        }
+
+        protected override void TriggerStart(){
+            anim = gameObject.GetComponent<Animator>();
         }
 
         public override void AttemptNeutralize()
@@ -19,8 +27,7 @@ namespace PropScripts
             // here add to resources
             BuoyResource.GetInstance().AddAmount(1);
             mapGenerator.buoyCount++;
-            
-            Destroy(gameManager);
+            anim.SetBool("PickedUp", true);
         }
 
         public override int MoneyToRemove { get; } = 0;
