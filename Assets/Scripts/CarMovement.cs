2c33faa302c79631c971891665bca2e04355fd0d
hrajmund <hubairajmund0507@gmail.com>
2023-07-08 12:37:17 +0200

    Collision Detection

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class CarMovement : MonoBehaviour
 {
     private Vector2 spawnPos;
+    private bool isCollided;
     public float ySpeed = 5.0f;
-    private List<RaycastHit2D> castCollision = new List<RaycastHit2D>();
     public Rigidbody2D rb;
     float scaleY;
 
     // Start is called before the first frame update
     void Start()
     {
+        isCollided = false;
         spawnPos = transform.position;
         scaleY = transform.localScale.y;
         rb = gameObject.GetComponent<Rigidbody2D>();
@@ -21,17 +22,21 @@ public class CarMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(spawnPos.y > 2){
-            rb.velocity = new Vector2(0.0f, -ySpeed);
-            transform.localScale = new Vector2(transform.localScale.x, (-1)*scaleY);
-        }
-        else if(spawnPos.y < -2){
-            rb.velocity = new Vector2(0.0f, ySpeed);
-            transform.localScale = new Vector2(transform.localScale.x, scaleY);
+        if(isCollided == false){
+            if(spawnPos.y > 2){
+                rb.velocity = new Vector2(0.0f, -ySpeed);
+                transform.localScale = new Vector2(transform.localScale.x, (-1)*scaleY);
+            }
+            else if(spawnPos.y < -2){
+                rb.velocity = new Vector2(0.0f, ySpeed);
+                transform.localScale = new Vector2(transform.localScale.x, scaleY);
+            }        
         }
     }
-/*
-    public bool Collision(){
-        
-    }*/
+
+    void OnTriggerEnter2D(Collider2D col){
+        rb.velocity = Vector2.zero;
+        isCollided = true;
+    }
+
 }
