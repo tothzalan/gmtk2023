1f56e0ddea77eb10da98ed77e791e16bdaca45f8
kaiser <kaiser.lacko@gmail.com>
2023-07-08 11:33:19 +0200

    Gen, props

---
+++ Assets/Scripts/PointOfInterest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointOfInterest : MonoBehaviour
+{
+    public bool dealerPoint; // police neut
+    public bool hookerPoint; // 
+    public bool trafficLightSpot; //
+    public bool carSpot; // buoy neut
+    public bool kebabSpot; // blackout neut (if opened)
+    public bool pubSpot;
+    public bool blackOutSpot;
+    public bool wetFloorSpot;
+    public bool busStationSpot;
+    public bool walkOnRoadSpot; // walks on the road and has to stop traffic with buoy
+    
+    public bool collectiblePoliceSpot;
+    public bool collectibleBuoySpot;
+    public bool collectibleStoreBlackout;
+}
