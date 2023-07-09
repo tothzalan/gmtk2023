b40b72434dd16b333f4b43456b5026fb8f02faf7
kaiser <kaiser.lacko@gmail.com>
2023-07-09 09:22:16 +0200

    virtual

--- Assets/Scripts/PropScripts/AbstractProp.cs
+++ Assets/Scripts/PropScripts/AbstractProp.cs
@@ -43,7 +43,7 @@ namespace PropScripts
             }
         }
 
-        public void FinalizeNeutralization()
+        public virtual void FinalizeNeutralization()
         {
             switch (gameManager.inventoryManager.UsingCurrently)
             {
