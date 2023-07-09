1a700c790c3ac1d55b1dec96586828832ac1abea
kaiser <kaiser.lacko@gmail.com>
2023-07-09 17:52:21 +0200

    Ez már amúgy sem kell

--- Assets/Scripts/PropScripts/TrafficLight.cs
+++ Assets/Scripts/PropScripts/TrafficLight.cs
@@ -1,68 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Enums;
-using PropScripts;
 
-public class TrafficLight : AbstractProp
+namespace PropScripts
 {
-
-    public override void AttemptNeutralize()
+    public class TrafficLight : AbstractProp
     {
-        if(state == TrafficLightState.Green)
-            state = TrafficLightState.Red;
-        else if(state == TrafficLightState.Red)
-            state = TrafficLightState.Green;
-        ChangeState();
-    }
 
-    public override void FinalizeNeutralization()
-    {
-    }
-
-    public override int MoneyToRemove { get { return 0; } }
-    public override int ToxicityDifference { get { return 0; } }
-    public override int ScoreDifference { get { return 0; } }
-
-
-    private TrafficLightState state;
-    private Color red = new Color(255, 0, 0);
-    private Color green = new Color(0, 255, 0);
-
-    private Rigidbody2D rigidBody;
-    private SpriteRenderer spriteRenderer;
-    private BoxCollider2D boxCollider;
-
-
-    void Start()
-    {
-        rigidBody = gameObject.GetComponent<Rigidbody2D>();
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        boxCollider = gameObject.GetComponent<BoxCollider2D>();
-        state = RandomStartState();
-        ChangeState();
-    }
-
-    TrafficLightState RandomStartState()
-    {
-        var rnd = Random.Range(0,10);
-        if(rnd <= 7)
-            return TrafficLightState.Green;
-        return TrafficLightState.Red;
-    }
-
-    void ChangeState()
-    {
-        if(state == TrafficLightState.Green)
+        public override void AttemptNeutralize()
         {
-            boxCollider.isTrigger = true;
-            spriteRenderer.color = green;
-
+            if(state == TrafficLightState.Green)
+                state = TrafficLightState.Red;
+            else if(state == TrafficLightState.Red)
+                state = TrafficLightState.Green;
+            ChangeState();
         }
-        else if(state == TrafficLightState.Red)
+
+        public override void FinalizeNeutralization()
         {
-            boxCollider.isTrigger = false;
-            spriteRenderer.color = red;
+        }
+
+        public override int MoneyToRemove { get { return 0; } }
+        public override int ToxicityDifference { get { return 0; } }
+        public override int ScoreDifference { get { return 0; } }
+
+
+        private TrafficLightState state;
+        private Color red = new Color(255, 0, 0);
+        private Color green = new Color(0, 255, 0);
+
+        private Rigidbody2D rigidBody;
+        private SpriteRenderer spriteRenderer;
+        private BoxCollider2D boxCollider;
+
+
+        protected override void TriggerStart()
+        {
+            rigidBody = gameObject.GetComponent<Rigidbody2D>();
+            spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            boxCollider = gameObject.GetComponent<BoxCollider2D>();
+            state = RandomStartState();
+            ChangeState();
+        }
+
+        TrafficLightState RandomStartState()
+        {
+            var rnd = Random.Range(0,10);
+            if(rnd <= 7)
+                return TrafficLightState.Green;
+            return TrafficLightState.Red;
+        }
+
+        void ChangeState()
+        {
+            if(state == TrafficLightState.Green)
+            {
+                boxCollider.isTrigger = true;
+                spriteRenderer.color = green;
+
+            }
+            else if(state == TrafficLightState.Red)
+            {
+                boxCollider.isTrigger = false;
+                spriteRenderer.color = red;
+            }
         }
     }
 }
