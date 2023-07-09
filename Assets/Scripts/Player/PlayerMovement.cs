91f01203a208b05eeec1a846f053e515af627e42
tothzalan <toth.zalan03@gmail.com>
2023-07-09 13:19:14 +0200

    Fixed PlayerMovement kinda

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,97 +6,73 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    private float horizontal;
-    private bool isCollided;
     public Animator animation;
     public GameObject carPrefab;
     private DeathScreen deathScreenScript;
     public GameObject deathScreen;
-    //private float speed = 8f;
+    private float speed = 8f;
 
-    private Rigidbody2D rb;
-    private GameManager gm;
-    //private CarMovement cm;
-    //private Spawner spawn;
+    private Rigidbody2D rigidBody;
+    private GameManager gameManager;
 
-    public float xAxisPlayer;
-    public float ySpawn;
-    public bool triggerEvent;
-
+    private bool inCollision = false;
     private bool dead = false;
 
     void Start() {
-        isCollided = false;
-        triggerEvent = false;
         animation.enabled = true;
-        rb = gameObject.GetComponent<Rigidbody2D>();
-        gm = GameObject.FindWithTag("GameController").GetComponent<GameManager>();
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        gameManager = GameObject.FindWithTag("GameController").GetComponent<GameManager>();
         deathScreenScript = deathScreen.GetComponent<DeathScreen>();
-        //spawn = GameObject.FindWithTag("SpawnTag").GetComponent<Spawner>();
-        //cm = GameObject.FindWithTag("Car").GetComponent<CarMovement>();
     }
 
     private void FixedUpdate(){
-        if(!isCollided && !dead){
-            if(!triggerEvent){
-                rb.velocity = new Vector2(((gm.SpeedMultiplier+1) * 0.1f), 0.0f);
-                xAxisPlayer = rb.position.x; 
-            }else{
-                rb.velocity = new Vector2(0.0f, 0.0f);
-            }
-        }
-
         if (dead)
         {
+            rigidBody.velocity = Vector2.zero;
             deathScreenScript.Death();
             animation.enabled = false;
+            return;
+        }
+        if(!inCollision)
+        {
+            rigidBody.velocity = new Vector2(((gameManager.SpeedMultiplier+1) * 0.1f * speed), 0.0f);
         }
     }
 
     void OnTriggerEnter2D(Collider2D col){
         var colTag = col.tag;
-        //Debug.Log("Fasz vagy");
         if(!dead)
         {
             if(colTag == "EnterCol")
             {
-                isCollided = true;
-                rb.velocity = new Vector2(((gm.SpeedMultiplier+1) * 0.1f), -0.8f);
-                GameObject car = Instantiate(carPrefab) as GameObject;
-                //Debug.Log(car.position.x);
-                car.transform.position = new Vector2(xAxisPlayer-15, 0.0f);            
+                inCollision = true;
+                gameObject.transform.position =
+                    Vector2.Lerp(
+                        gameObject.transform.position, 
+                        new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 5),
+                        Time.deltaTime * 10);
             }
-            else if(colTag == "StayCol")
-            {
-                isCollided = true;
-                rb.velocity = new Vector2(((gm.SpeedMultiplier+1) * 0.1f), 0.0f);
-            }
+
             else if(colTag == "ExitCol")
             {
-                isCollided = true;
-                rb.velocity = new Vector2(((gm.SpeedMultiplier+1) * 0.1f), 0.8f);
+                inCollision = true;
+                gameObject.transform.position =
+                    Vector2.Lerp(
+                        gameObject.transform.position, 
+                        new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 5),
+                        Time.deltaTime * 10);
             }
             else if(colTag == "Car")
             {
-                // TODO: play cool death animation
-                rb.velocity = new Vector2(0, 0);
-                gm.Dead = true;
                 dead = true;
+                gameManager.Dead = true;
             }
         }
     }
     
-    void OnTriggerExit2D(Collider2D col){
-        var colTag = col.tag;       
-        if(colTag == "EnterCol"){
-            isCollided = false;
-            Debug.Log("Faszszopo vagy");
-
-        }else if(colTag == "ExitCol"){
-            isCollided = false;
-            rb.velocity = new Vector2(((gm.SpeedMultiplier+1) * 0.1f), 0.0f);
-        }else if(colTag == "StayCool"){
-            isCollided = false;
-        }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.tag == "ExitCol") 
+            inCollision = false;
     }
 }
